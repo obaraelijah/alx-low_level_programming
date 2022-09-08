29aec26989e0d92d594ad7ccd413404f72f9d16b
obaraelijah <elijahobara357@gmail.com>
2022-09-08 08:43:54 +0000

    Correcting task error 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main -entry point
+ *
+ * Return: Always zaro(0)(success)
+ */
+
 int main(void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

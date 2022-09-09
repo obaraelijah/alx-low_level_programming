6a26fdb627115d82b9709953f84572a8136cc4a7
obaraelijah <elijahobara357@gmail.com>
2022-09-09 08:43:44 +0000

    Tasks

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+				{
+					putchar((a % 10) + '0');
+					putchar((b % 10) + '0');
+					putchar((c % 10) + '0');
+
+					if (a == '7' && b == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	char low;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (low = 'a'; low <+ 'g'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
+

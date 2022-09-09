6839916c333b51ac75610936c9f36922a257592a
obaraelijah <elijahobara357@gmail.com>
2022-09-09 14:10:38 +0000

    More error corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,12 @@
 
 int main(void)
 {
-	int a,b;
+	int a, b;
 
-	for (a = '0'; a  < '9'; a++)
+	for (a = '0'; a < '9'; a++)
 	{
 		for (b = a + 1; b <= '9'; b++)
 		{
-			if(a != b)
 			{
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');

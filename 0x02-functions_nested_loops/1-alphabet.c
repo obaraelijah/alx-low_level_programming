2da264ed575ada2688a5597bb3594aafd570e375
obaraelijah <elijahobara357@gmail.com>
2022-09-14 12:48:23 +0000

    Did some corrections on my code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,10 @@ nclude " main.h"
  */
 void print_alphabet(void)
 {
-	char i;
+	char letter;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }

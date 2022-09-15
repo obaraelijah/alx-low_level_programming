4bd080d59e6ba12ea6eb4742da1f2ecc9fcc0f05
obaraelijah <elijahobara357@gmail.com>
2022-09-15 05:00:51 +0000

    Task corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,6 +10,7 @@ void print_alphabet(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-
-	_putchar('\n');
+	{
+	_putchar("\n");
+	}
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,8 +8,5 @@
  */
 int _abs(int r);
 {
-	if (r >= 0)
-		return (r);
-	else
-		return (r * -1);
+	return(r >= 0 ? r : r * -1);
 }

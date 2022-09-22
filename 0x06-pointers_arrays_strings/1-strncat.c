942acbd3e92e31737d25abaa299532183f25f6b7
obaraelijah <elijahobara357@gmail.com>
2022-09-22 13:56:07 +0000

    Error correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,26 @@
   */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0; dlen = 0;
+	int srclen = 0, i = 0;
+	char *tmp = dest, *start = src;
 
-	while (dest[index++])
-		dlen++;
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dlen++] = src[index];
+	while (*dest)
+		dest++;
 
-	return (dest);
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (tmp);
 }

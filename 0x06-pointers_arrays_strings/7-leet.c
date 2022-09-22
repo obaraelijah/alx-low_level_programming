4144853b3e6755e8eea1c7923788fd3306f72d3b
obaraelijah <elijahobara357@gmail.com>
2022-09-22 15:25:50 +0000

    Task correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,26 +7,26 @@
   */
 char *leet(char *s)
 {
-int a = 0, b = 0, l = 5;
-char r[5] = {A, E, O, T, L};
-char n[5] = {4, 3, 0, 7, 1};
+	int a = 0, b = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
 
-while (s[a])
-{
-b = 0;
+	while (s[a])
+	{
+		b = 0;
 
-while (b < l)
-{
-if (s[a] == r[b] || s[a] - 32 == r[b])
-{
-s[a] = n[b];
-}
+		while (b < l)
+		{
+			if (s[a] == r[b] || s[a] - 32 == r[b])
+			{
+				s[a] = n[b];
+			}
 
-b++;
-}
+			b++;
+		}
 
-a++;
-}
+		a++;
+	}
 
-return (s);
+	return (s);
 }

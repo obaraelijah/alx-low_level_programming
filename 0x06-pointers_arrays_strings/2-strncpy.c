821ab553deadd8e26ae2fd17f05a147e38ccd8c2
obaraelijah <elijahobara357@gmail.com>
2022-09-25 14:42:02 +0000

    Fixed code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,15 +8,7 @@
   * Return: char value
   */
 char *_strncpy(char *dest, char *src, int n)
-char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-
-for (i = 0; i < n && src[i] != 0; i++)
-dest[i] = src[i];
-
-for ( ; i < n; i++)
-dest[i] = 0;
-
-return (dest);
+	strncpy(dest, src, n);
+	return (dest);
 }

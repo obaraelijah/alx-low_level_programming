c5accb97aa4c153860abaa0cf9beeaaa33ade348
obaraelijah <elijahobara357@gmail.com>
2022-10-03 07:12:19 +0000

    Advanced task error correction

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,46 +6,31 @@
  * Return: Pointer to new string
  **/
 char *argstostr(int ac, char **av)
-char *argstostr(int ac, char **av)
 {
-	int ch = 0, i = 0, j = 0, k = 0;
-	char *s;
+char *cont;
+int i;
+int j;
+int len = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	while (i < ac)
-	{
-		while (av[i][j])
-		{
-			ch++;
-			j++;
-		}
-
-		j = 0;
-		i++;
-	}
-
-	s = malloc((sizeof(char) * ch) + ac + 1);
-
-	i = 0;
-	while (av[i])
-	{
-		while (av[i][j])
-		{
-			s[k] = av[i][j];
-			k++;
-			j++;
-		}
-
-		s[k] = '\n';
-
-		j = 0;
-		k++;
-		i++;
-	}
-
-	k++;
-	s[k] = '\0';
-	return (s);
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+j = 0;
+while (av[i][j++])
+len++;
 }
+len++;
+cont = malloc(sizeof(**av) * (len + ac));
+if (cont == NULL)
+return (NULL);
+len = 0;
+for (i = 0; i < ac; i++)
+{
+j = 0;
+while (av[i][j])
+cont[len++] = av[i][j++];
+cont[len++] = n;
+}
+cont[len] = 0;
+return (cont);

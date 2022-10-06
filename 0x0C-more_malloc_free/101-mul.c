953276a8cd7ff0261ddf9b68ef8a2337ecc445fe
obaraelijah <elijahobara357@gmail.com>
2022-10-06 09:02:08 +0000

    Last advanced task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Check the code
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Nothing
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+		return (0);
+	}
+}

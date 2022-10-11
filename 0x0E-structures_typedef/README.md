296ff39db6f95f765d56f46b7ff14610b704164c
obaraelijah <elijahobara357@gmail.com>
2022-10-11 05:30:27 +0000

    Struct exercises

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Learning C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef _HEADER__
+#define _HEADER_
+
+/**
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description:The attributes of a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/** 
+ * dog_t - Typedef for dog structure.
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
